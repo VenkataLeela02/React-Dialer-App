daf57e3a2bda8d1abbb63afc30595d015290764f
venkataleela02 <a.leelateja@gmail.com>
2021-09-27 22:18:09 +0530

    React dialer repository added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [state,setState] = useState("");
+  const [text,setText] = useState("");
+  const [indexVal,setIndex] = useState(1);
+
+  const array = [[",",".","!"],["a","b","c"],["d","e","f"],["g","h","i"],["j","k","l"],["m","n","o"],["p","q","r","s"],["t","u","v"],["w","x","y","z"],["*"],["0"],["#"]];
+  
+  const clickHandle = (e,index) => {
+    setIndex(index);
+    e.preventDefault();
+    e.stopPropagation();
+    setCount(count+1);
+    return indexVal;
+  } 
+
+  useEffect(() => {
+    
+    const stateInt = setInterval(() => {setCount(0);setIndex(indexVal+1);setState(array[indexVal][count]);},210);
+    
+    const textChange = setTimeout(() => {setText(text+state)},1000);
+    return () => {clearInterval(stateInt,textChange)};
+
+  }, [count,setCount],[state,setState],indexVal);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <input type="text" className="text" defaultValue={text.replace(/,/g,'')}></input>
+          {array.map((buttonId,index) => (
+          <button
+            key={index}
+            value={index}
+            onClick={(event) => clickHandle(event,index)}
+            /*onMouseUp= {(event) => {setText(text+(index+1));}}*/
+            useref={index}
+          >
+            {(index<9)? index+1 : ""}<br></br>{buttonId}
+          </button>
+           ))}
+          
+        </div>
       </header>
     </div>
   );
